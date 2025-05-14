7417d0de079ed42f3278d4bb39e9eeceaca3fd3a
Rohit Dhiman <129442036+RohitDhiman7769@users.noreply.github.com>
2025-05-14 16:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import connectDB from './db.js';
 import router from './src/router/movies-routes.js';
 import moviesScheema from './src/module/movies-details.js';
-
+import cors from 'cors'
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/', router)
 
 try {
